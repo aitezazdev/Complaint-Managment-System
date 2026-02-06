311afc745a629dfcaa6f5f6acafc96227376e559
aitezazdev <aitezazsikandar@gmail.com>
2026-02-07 01:41:50 +0500

    form handling

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import { connectDB } from "./config/db.js";
 import { configDotenv } from "dotenv";
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 configDotenv();
 
